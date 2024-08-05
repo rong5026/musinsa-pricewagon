330e93ab688bfa212b8f956bb0b4263422384f8e
rong5026 <rong5026@naver.com>
2024-08-05 17:48:41 +0900

    feat sqlalchemy로 mysql 상품데이터 추가

--- individual-product.py
+++ individual-product.py
@@ -7,12 +7,68 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from driver_setup import setup_driver  
 from multiprocessing import Pool, cpu_count
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import datetime
 
 
-def extract_product_info(soup, product_url):
+Base = declarative_base()
+load_dotenv() # 환경변수 로딩
+
+# 무신사 상품 기본 URL
+MUSINSA_PRODUCT_URL = os.getenv("MUSINSA_PRODUCT_URL")
+
+# MySQL
+DB_NAME = os.getenv("DB_NAME")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PASSWORD =os.getenv("MYSQL_PASSWORD")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_USERNAME = os.getenv("MYSQL_USERNAME")
+
+# SQLAlchemy 설정
+DATABASE_URI = f'mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{DB_NAME}'
+engine = create_engine(DATABASE_URI, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Product(Base):
+    __tablename__ = 'product'
+    
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    category_id = Column(Integer, nullable=False)
+    product_id = Column(Integer, unique=True, nullable=False)
+    img_url = Column(String(200))
+    name = Column(String(100))
+    brand = Column(String(100))
+    product_url = Column(String(200))
+    current_price = Column(Integer)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+# 테이블 생성
+Base.metadata.create_all(engine)
+
+def save_to_database(products_info):
+    try:
+        with session.begin():
+            for product in products_info:
+                new_product = Product(
+                    name=product['name'],
+                    brand=product['brand'],
+                    category_id=1,  # 예시로 카테고리 ID를 1로 설정
+                    product_id=int(product['product_id']),
+                    img_url=product['image_url'],
+                    product_url=product['product_url'],
+                    current_price=int(product['current_price']) if product['current_price'] != 'N/A' else 0
+                )
+                session.add(new_product)
+    except Exception as e:
+        session.rollback()
+        print(f"초기 상품 저장 오류: {e}")
+    
+def extract_product_info(soup, product_num):
     name_tag = soup.find('h2', class_='sc-1pxf5ii-2 fIpPKc')
     name = name_tag.get_text(strip=True) if name_tag else 'N/A'
 
@@ -35,16 +91,17 @@ def extract_product_info(soup, product_url):
     like_count = ''.join(filter(str.isdigit, like_text))  # 숫자만 추출
 
     return {
-        'Name': name,
-        'Brand': brand,
-        'Product_URL' : product_url,
-        'Price': price,
-        'Like': like_count,
-        'Image_URL': img_url
+        'name': name,
+        'brand': brand,
+        'product_id' : product_num,
+        'current_price': price,
+        'like_count': like_count,
+        'image_url': img_url,
+        'product_url' : MUSINSA_PRODUCT_URL + "/" +product_num,
     }
 
 def get_individual_product_info(chromedriver_path, product_num):
-    product_url = f'https://www.musinsa.com/app/goods/{product_num}'
+    product_url = f'{MUSINSA_PRODUCT_URL}/{product_num}'
     driver = setup_driver(chromedriver_path)
     
     try:
@@ -59,7 +116,7 @@ def get_individual_product_info(chromedriver_path, product_num):
         html = driver.page_source
         soup = BeautifulSoup(html, 'lxml')
 
-        product_info = extract_product_info(soup, product_url)
+        product_info = extract_product_info(soup, product_num)
     
         return product_info
 
@@ -69,7 +126,7 @@ def get_individual_product_info(chromedriver_path, product_num):
 def fetch_product_info_multithread(products_num, chromedriver_path):
     products_info = []  # 상품 정보를 저장할 리스트
     with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(get_individual_product_info, chromedriver_path, product) for product in products_num]
+        futures = [executor.submit(get_individual_product_info, chromedriver_path, product_num) for product_num in products_num]
         for future in futures:
             product_info = future.result()
             products_info.append(product_info)  # 결과를 리스트에 추가
@@ -85,12 +142,14 @@ def fetch_product_info_multiprocess(products_num, chromedriver_path):
 def print_product_data(products_info):
     # 결과 출력
     for product_info in products_info:
-        print(f'상품 이름: {product_info["Name"]}')
-        print(f'브랜드: {product_info["Brand"]}')
-        print(f'상품 가격: {product_info["Price"]}')
-        print(f'상품 URL: {product_info["Product_URL"]}')
-        print(f'상품 이미지 URL: {product_info["Image_URL"]}')
-        print(f'좋아요 수: {product_info["Like"]}')
+        
+        print(f'상품 이름: {product_info["name"]}')
+        print(f'브랜드: {product_info["brand"]}')
+        print(f'상품 가격: {product_info["current_price"]}')
+        print(f'상품 URL: {product_info["product_url"]}')
+        print(f'상품 이미지 URL: {product_info["image_url"]}')
+        print(f'좋아요 수: {product_info["like_count"]}')
+        print("---------------------------------------")
 
 def read_product_numbers(file_path):
     with open(file_path, 'r') as file:
@@ -109,5 +168,7 @@ def main():
     end_time = time.time()
     print(f'총 실행 시간: {end_time - start_time:.2f}초')  # 실행 시간 계산 및 출력
 
+    save_to_database(product_info)
+    
 if __name__ == "__main__":
     main()
