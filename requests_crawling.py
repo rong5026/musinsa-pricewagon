0c73bbe62d4198c21551df37f54499ec3daa61ae
rong5026 <rong5026@naver.com>
2024-08-06 22:20:57 +0900

    feat 정적크롤링 멀티스레드 적용

--- requests_crawling.py
+++ requests_crawling.py
@@ -4,9 +4,9 @@ import json
 import os
 from dotenv import load_dotenv
 import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-
-load_dotenv() # 환경변수 로딩
+load_dotenv()  # 환경변수 로딩
 
 # 무신사 상품 기본 URL
 MUSINSA_PRODUCT_URL = os.getenv("MUSINSA_PRODUCT_URL")
@@ -19,22 +19,25 @@ def read_product_numbers(file_path):
 def fetch_price(product_id, headers):
     product_url = f'{MUSINSA_PRODUCT_URL}/{product_id}'
 
-    response = requests.get(product_url, headers=headers)
+    try:
+        response = requests.get(product_url, headers=headers)
+        response.raise_for_status()  # 상태 코드가 200이 아니면 예외 발생
 
-    if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'lxml')
         script_tag = soup.find('script', type='application/ld+json')
         
         if script_tag:
             json_data = json.loads(script_tag.string)
             price = json_data.get('offers', {}).get('price', 'N/A')
-            return price
+            return product_id, price
         else:
             print(f'가격 정보를 찾을 수 없습니다. 상품 번호: {product_id}')
-            return 'N/A'
-    else:
-        print(f'페이지를 불러오지 못했습니다. 상품 번호: {product_id} 상태 코드: {response.status_code}')
-        return 'N/A'
+            return product_id, 'N/A'
+    except requests.RequestException as e:
+        print(f'페이지를 불러오지 못했습니다. 상품 번호: {product_id} 오류: {e}')
+        return product_id, 'N/A'
+    finally:
+        response.close()  # 연결 종료
 
 def main():
     products_file = './etc/individual_products.txt'
@@ -48,13 +51,16 @@ def main():
     
     start_time = time.time()  # 시작 시간 기록
     
-    for product_id in products_num:
-        price = fetch_price(product_id, headers)
-        print(f'상품 번호: {product_id}, 상품 가격: {price}원')
+    # 멀티스레딩 사용
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(fetch_price, product_id, headers) for product_id in products_num]
+        
+        for future in as_completed(futures):
+            product_id, price = future.result()
+            print(f'상품 번호: {product_id}, 상품 가격: {price}원')
         
     end_time = time.time()
     print(f'총 실행 시간: {end_time - start_time:.2f}초')  # 실행 시간 계산 및 출력
-
 
 if __name__ == "__main__":
     main()
