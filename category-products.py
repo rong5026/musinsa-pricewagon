f7f7e6c2b6d6accacafc6df58e85ef3e6aeb7bb3
rong5026 <rong5026@naver.com>
2024-08-07 21:12:08 +0900

    issue template

--- category-products.py
+++ category-products.py
@@ -1,77 +1,80 @@
-import os
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-import pandas as pd
-from bs4 import BeautifulSoup
-import re
-from driver_setup import setup_driver  
+# import os
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# import pandas as pd
+# from bs4 import BeautifulSoup
+# import re
+# from driver_setup import setup_driver  
 
-def extract_product_info(product):
-    name_tag = product.select_one('div[class*="category__sc-rb2kzk-10"] a:nth-of-type(2)')
-    name = name_tag.get_text(strip=True) if name_tag else 'N/A'
+# def extract_product_info(product):
+#     name_tag = product.select_one('div[class*="category__sc-rb2kzk-10"] a:nth-of-type(2)')
+#     name = name_tag.get_text(strip=True) if name_tag else 'N/A'
 
-    brand_tag = product.select_one('div[class*="category__sc-rb2kzk-10"] a:nth-of-type(1)')
-    brand = brand_tag.get_text(strip=True) if brand_tag else 'N/A'
+#     brand_tag = product.select_one('div[class*="category__sc-rb2kzk-10"] a:nth-of-type(1)')
+#     brand = brand_tag.get_text(strip=True) if brand_tag else 'N/A'
 
-    price_tag = product.select_one('div[class*="category__sc-79f6w4-4"] span')
-    price = price_tag.get_text(strip=True).replace(',', '').split('원')[0] if price_tag else 'N/A'
+#     price_tag = product.select_one('div[class*="category__sc-79f6w4-4"] span')
+#     price = price_tag.get_text(strip=True).replace(',', '').split('원')[0] if price_tag else 'N/A'
 
-    like_tag = product.select_one('div[class*="category__sc-rb2kzk-15"]')
-    if like_tag:
-        like_text = like_tag.get_text(strip=True).replace(',', '')
-        like = re.search(r'\d+', like_text).group() if re.search(r'\d+', like_text) else 'N/A'
-    else:
-        like = 'N/A'
+#     like_tag = product.select_one('div[class*="category__sc-rb2kzk-15"]')
+#     if like_tag:
+#         like_text = like_tag.get_text(strip=True).replace(',', '')
+#         like = re.search(r'\d+', like_text).group() if re.search(r'\d+', like_text) else 'N/A'
+#     else:
+#         like = 'N/A'
         
-    img_tag = product.select_one('img')
-    img_url = img_tag['src'] if img_tag else 'N/A'
+#     img_tag = product.select_one('img')
+#     img_url = img_tag['src'] if img_tag else 'N/A'
 
-    return {
-        'Name': name,
-        'Brand': brand,
-        'Price': price,
-        'Like' : like,
-        'Image_URL': img_url
-    }
+#     return {
+#         'Name': name,
+#         'Brand': brand,
+#         'Price': price,
+#         'Like' : like,
+#         'Image_URL': img_url
+#     }
 
-def scroll_and_load_products(driver, target_count=100):
-    product_list = []
-    while len(product_list) < target_count:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)  # 새로운 상품이 로드될 때까지 잠시 대기
+# def scroll_and_load_products(driver, target_count=100):
+#     product_list = []
+#     while len(product_list) < target_count:
+#         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#         time.sleep(2)  # 새로운 상품이 로드될 때까지 잠시 대기
 
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'lxml')
-        products = soup.select('div.category__sc-rb2kzk-0.irgXw')
+#         html = driver.page_source
+#         soup = BeautifulSoup(html, 'lxml')
+#         products = soup.select('div.category__sc-rb2kzk-0.irgXw')
         
-        new_products = [extract_product_info(product) for product in products[len(product_list):]]
-        product_list.extend(new_products)
+#         new_products = [extract_product_info(product) for product in products[len(product_list):]]
+#         product_list.extend(new_products)
 
-        if len(new_products) == 0:
-            break  # 더 이상 로드할 상품이 없으면 종료
+#         if len(new_products) == 0:
+#             break  # 더 이상 로드할 상품이 없으면 종료
     
-    return product_list[:target_count]
+#     return product_list[:target_count]
 
-def main():
-    url = 'https://www.musinsa.com/categories/item/002025?device=mw&sortCode=1y'
-    item_count = 100 # 크롤링 할 상품의 수
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    chromedriver_path = os.path.join(current_dir, 'chromedriver')
-    driver = setup_driver(chromedriver_path)
+# def main():
+#     url = 'https://www.musinsa.com/categories/item/002025?device=mw&sortCode=1y'
+#     item_count = 100 # 크롤링 할 상품의 수
+#     current_dir = os.path.dirname(os.path.abspath(__file__))
+#     chromedriver_path = os.path.join(current_dir, 'chromedriver')
+#     driver = setup_driver(chromedriver_path)
     
-    try:
-        driver.get(url)
-        time.sleep(5)  # 초기 페이지 로드 대기
+#     try:
+#         driver.get(url)
+#         time.sleep(5)  # 초기 페이지 로드 대기
 
-        product_list = scroll_and_load_products(driver, item_count)
+#         product_list = scroll_and_load_products(driver, item_count)
 
-        df = pd.DataFrame(product_list)
-        print(df)
-        df.to_excel('musinsa_top_100_products.xlsx', index=False)
+#         df = pd.DataFrame(product_list)
+#         print(df)
+#         df.to_excel('musinsa_top_100_products.xlsx', index=False)
     
-    finally:
-        driver.quit()
+#     finally:
+#         driver.quit()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+print("asdfsdf")
--- requests_crawling.py
+++ requests_crawling.py
@@ -4,9 +4,8 @@ import json
 import os
 from dotenv import load_dotenv
 import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
-load_dotenv()  # 환경변수 로딩
+load_dotenv() 
 
 # 무신사 상품 기본 URL
 MUSINSA_PRODUCT_URL = os.getenv("MUSINSA_PRODUCT_URL")
@@ -19,48 +18,41 @@ def read_product_numbers(file_path):
 def fetch_price(product_id, headers):
     product_url = f'{MUSINSA_PRODUCT_URL}/{product_id}'
 
-    try:
-        response = requests.get(product_url, headers=headers)
-        response.raise_for_status()  # 상태 코드가 200이 아니면 예외 발생
+    response = requests.get(product_url, headers=headers)
 
+    if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'lxml')
         script_tag = soup.find('script', type='application/ld+json')
         
         if script_tag:
             json_data = json.loads(script_tag.string)
             price = json_data.get('offers', {}).get('price', 'N/A')
-            return product_id, price
+            return price
         else:
             print(f'가격 정보를 찾을 수 없습니다. 상품 번호: {product_id}')
-            return product_id, 'N/A'
-    except requests.RequestException as e:
-        print(f'페이지를 불러오지 못했습니다. 상품 번호: {product_id} 오류: {e}')
-        return product_id, 'N/A'
-    finally:
-        response.close()  # 연결 종료
+            return 'N/A'
+    else:
+        print(f'페이지를 불러오지 못했습니다. 상품 번호: {product_id} 상태 코드: {response.status_code}')
+        return 'N/A'
 
 def main():
     products_file = './etc/individual_products.txt'
     products_num = read_product_numbers(products_file)
     
-    # 요청 헤더 설정 (User-Agent는 웹 크롤링 방지 설정을 우회하기 위해 필요)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         "Connection": "close"
     }
     
-    start_time = time.time()  # 시작 시간 기록
+    start_time = time.time()  
     
-    # 멀티스레딩 사용
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(fetch_price, product_id, headers) for product_id in products_num]
-        
-        for future in as_completed(futures):
-            product_id, price = future.result()
-            print(f'상품 번호: {product_id}, 상품 가격: {price}원')
+    for product_id in products_num:
+        price = fetch_price(product_id, headers)
+        print(f'상품 번호: {product_id}, 상품 가격: {price}원')
         
     end_time = time.time()
-    print(f'총 실행 시간: {end_time - start_time:.2f}초')  # 실행 시간 계산 및 출력
+    print(f'총 실행 시간: {end_time - start_time:.2f}초') 
+
 
 if __name__ == "__main__":
     main()
