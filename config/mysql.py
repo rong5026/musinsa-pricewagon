b296bf93d748baeea110409540329c21a4124a1d
rong5026 <rong5026@naver.com>
2024-08-12 20:24:35 +0900

    feat 상품 생성 시 musinsa 테이블 생성

--- config/mysql.py
+++ config/mysql.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, BigInteger, Enum, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, BigInteger, ForeignKey, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import NoResultFound
@@ -44,28 +44,84 @@ class Category(Base):
     category_name = Column(String(50), nullable=False, unique=True)
 
 
-# 테이블 생성
+class Musinsa(Base):
+    __tablename__ = 'musinsa'
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    product_id = Column(BigInteger, ForeignKey('product.id'), nullable=False)
+    rating = Column(Float, nullable=True)
+    review_count = Column(Integer, nullable=True)
+    like_count = Column(Integer, nullable=True)
+
 Base.metadata.create_all(engine)
 
+def create_product(product):
+    try:
+        product_name = product['name']
+        brand = product['brand']
+        product_id = int(product['product_id'])  
+        category_id = get_or_create_category(product['category'], product['parent_category'])
+        img_url=product['image_url'],
+        product_url=product['product_url'],
+        current_price=int(product['current_price']) if product['current_price'] != 'N/A' else 0
+        
+        # Product 객체 생성
+        new_product = Product(
+            name=product_name,
+            brand=brand,
+            category_id=category_id,
+            product_id=product_id,
+            img_url=img_url,
+            product_url=product_url,
+            current_price=current_price
+        )
+        return new_product
 
-def save_to_database(products_info):
+    except KeyError as e:
+        logging.error(f"product 생성 중 상 누락 발생: {e}")
+        raise
+
+    except Exception as e:
+        logging.error(f"상품 생성 중 오류 발생: {e}")
+        return None
+    
+def create_musinsa(product, new_product_id):
+    try:
+        rating = float(product.get('star_count', 0.0))
+        review_count = int(product.get('review_count', 0))
+        like_count = int(product.get('like_count', 0))
+
+        return Musinsa(
+            product_id=new_product_id,  
+            rating=rating,
+            review_count=review_count,
+            like_count=like_count
+        )
+    except (ValueError, TypeError) as e:
+        logging.error(f"Musinsa 생성 중 데이터 변환 오류 발생: {e} - 데이터: {product}")
+        return None
+    
+    except Exception as e:
+        logging.error(f"Musinsa 생성 중 알 수 없는 오류 발생: {e}")
+        return None
+    
+def save_product_info(products_info):
     session = Session()
     try:
         with session.begin():
             for product in products_info:
-                new_product = Product(
-                    name=product['name'],
-                    brand=product['brand'],
-                    category_id= get_or_create_category(product['category'], product['parent_category']), 
-                    product_id=int(product['product_id']),
-                    img_url=product['image_url'],
-                    product_url=product['product_url'],
-                    current_price=int(product['current_price']) if product['current_price'] != 'N/A' else 0
-                )
+                # Product 생성
+                new_product = create_product(product)
                 session.add(new_product)
+                session.flush()  # ID를 얻기 위해 flush 수행
+                
+                new_musinsa = create_musinsa(product, new_product.id)
+                session.add(new_musinsa)
     except Exception as e:
         session.rollback()
         logging.error(f"초기 상품 저장 오류: {e}")
+    finally:
+        session.close()
 
 def get_or_create_category(category_name, parent_category_name=None):
     session = Session()
@@ -87,5 +143,7 @@ def get_or_create_category(category_name, parent_category_name=None):
         session.add(new_category)
         session.commit()
         category = new_category
-    
+    finally:
+        session.close()
+        
     return category.id
