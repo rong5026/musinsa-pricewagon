bee10160745a91132fb35e4cdd17d6defd6a77ef
rong5026 <rong5026@naver.com>
2024-08-30 20:39:44 +0900

    카테고리 id로 상품들의 id값들 가져오기

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from musinsa.product_info_crawling import get_musinsa_product_info
 from musinsa.product_day_price import get_product_price
-
+from musinsa.products_num_by_category import extract_product_num_from_categoryinfo
 def main():
     get_musinsa_product_info()
+    # extract_product_num_from_categoryinfo()
     # get_product_price()
     
 
--- musinsa/products_num_by_category.py
+++ musinsa/products_num_by_category.py
@@ -1,50 +1,31 @@
-import os
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import pandas as pd
-from bs4 import BeautifulSoup
-import re
-from config.driver_setup import setup_driver   
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import os
+import time
 from dotenv import load_dotenv
+from config.driver_setup import setup_driver
+import os
 
-load_dotenv() # 환경변수 로딩
+# 현재 스크립트의 디렉터리 경로 가져오기
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 상대 경로를 기반으로 파일의 절대 경로 생성
+file_path = os.path.join(script_dir, '..', 'musinsa_product_ids.txt')
+
+# 파일이 저장될 디렉터리가 존재하지 않으면 생성
+# os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+load_dotenv()  # 환경변수 로딩
 
 CHROMEDRIVER_PATH = os.getenv("CHROMEDRIVER_PATH")
 
-def extract_product_info(product):
-    product_id = product.get('data-goodsno', 'N/A')
-    
-    name_tag = product.select_one('div[class*="category__sc-rb2kzk-10"] a:nth-of-type(2)')
-    name = name_tag.get_text(strip=True) if name_tag else 'N/A'
-
-    brand_tag = product.select_one('div[class*="category__sc-rb2kzk-10"] a:nth-of-type(1)')
-    brand = brand_tag.get_text(strip=True) if brand_tag else 'N/A'
-
-    price_tag = product.select_one('div[class*="category__sc-79f6w4-4"] span')
-    price = price_tag.get_text(strip=True).replace(',', '').split('원')[0] if price_tag else 'N/A'
-
-    like_tag = product.select_one('div[class*="category__sc-rb2kzk-15"]')
-    if like_tag:
-        like_text = like_tag.get_text(strip=True).replace(',', '')
-        like = re.search(r'\d+', like_text).group() if re.search(r'\d+', like_text) else 'N/A'
-    else:
-        like = 'N/A'
-        
-    img_tag = product.select_one('img')
-    img_url = img_tag['src'] if img_tag else 'N/A'
-
-    return {
-        'Product_id': product_id,
-        'Name': name,
-        'Brand': brand,
-        'Price': price,
-        'Like' : like,
-        'Image_URL': img_url
-    }
-    
 def get_products_id(product):
-    product_id = product.get('data-goodsno', 'N/A')
+    # product_html = product.get_attribute('outerHTML')
+    # print(product_html)
+    product_id = product.get_attribute('data-item-id') 
+    
     return {
         'Product_ID': product_id
     }
@@ -55,17 +36,14 @@ def scroll_and_load_products(driver, url, target_count=100):
     driver.get(url)
     time.sleep(2)
     while len(product_list) < target_count:
-       
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)  # 새로운 상품이 로드될 때까지 잠시 대기
 
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'lxml')
-        products = soup.select('div.category__sc-rb2kzk-0.irgXw')
+        products = driver.find_elements(By.CSS_SELECTOR, 'a[data-item-id]')
         
+        # 새로운 상품만 추가
         new_products = [get_products_id(product) for product in products[len(product_list):]]
         product_list.extend(new_products)
-        
 
         if len(new_products) == 0:
             break  # 더 이상 로드할 상품이 없으면 종료
@@ -73,10 +51,10 @@ def scroll_and_load_products(driver, url, target_count=100):
     return product_list[:target_count]
 
 def extract_product_num_from_categoryinfo():
-    
     base_url = 'https://www.musinsa.com/categories/item/'
-    category_ids = ['001005', '001002', '001004', '001006','001003', '001010', '001001','001011', '001013', '001008']  
-    item_count = 120 # 크롤링 할 상품의 수
+    category_ids = ['003002', '003004', '002025', '002019']  # 크롤링 할 카테고리 ID
+    # category_ids = ['003002']
+    item_count = 10  # 크롤링 할 상품의 수
     chromedriver_path = f'{CHROMEDRIVER_PATH}'
     driver = setup_driver(chromedriver_path)
     
@@ -85,7 +63,7 @@ def extract_product_num_from_categoryinfo():
     
     try:
         for category_id in category_ids:
-            url = f'{base_url}{category_id}?device=mw&sortCode=1y'
+            url = f'{base_url}{category_id}?gf=A&sortCode=SALE_ONE_YEAR_COUNT'
             print(f'Crawling URL: {url}')
             
             product_list = scroll_and_load_products(driver, url, item_count)
@@ -95,14 +73,11 @@ def extract_product_num_from_categoryinfo():
             for product in product_list:
                 all_ids.append(product['Product_ID'])
 
-        # DataFrame 저장
-        df = pd.DataFrame(all_products)
-        df.to_excel('musinsa_top_100_products.xlsx', index=False)
-
         # 상품 ID를 텍스트 파일에 숫자만 저장
-        with open('musinsa_product_ids.txt', 'w', encoding='utf-8') as f:
+        with open(file_path, 'w', encoding='utf-8') as f:
             for product_id in all_ids:
                 f.write(f"{product_id}\n")
     
     finally:
         driver.quit()
+
