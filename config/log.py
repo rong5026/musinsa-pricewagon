bc252df8ecef426f167e39579d1e6cc52fe7b292
rong5026 <rong5026@naver.com>
2024-09-03 14:28:02 +0900

    gitaction test

--- config/log.py
+++ config/log.py
@@ -8,6 +8,10 @@ load_dotenv()
 # 로그 파일 경로 설정
 LOG_FILE = os.getenv("LOG_FILE", "application.log")
 
+# 로그 파일이 없으면 생성
+if not os.path.exists(LOG_FILE):
+    open(LOG_FILE, 'a').close()
+    
 # 로깅 설정
 logging.basicConfig(
     level=logging.INFO,
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,18 @@
 # 기존 컨테이너가 존재하면 삭제
 docker rm day-price-container --force || true
 
+# 로그 파일 경로 설정
+LOG_FILE="$HOME/app/log/cron.log"
+
+# 로그 파일이 없으면 생성
+if [ ! -f "$LOG_FILE" ]; then
+  mkdir -p $HOME/app/log
+  touch "$LOG_FILE"
+fi
 
 # cronjob 추가 (오전 6시 실행)
 # echo "0 6 * * * root python /app/main.py >> /app/log/cron.log 2>&1" >> /etc/crontab
-echo "* * * * * root python /app/main.py >> /app/log/cron.log 2>&1" >> /etc/crontab
+echo "* * * * * root python /app/main.py >> $HOME/app/log/cron.log 2>&1" >> /etc/crontab
 
 # cron 데몬 시작
-cron && tail -f /app/log/cron.log
+cron && tail -f "$LOG_FILE"
