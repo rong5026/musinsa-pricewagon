b1f85d45dbf34749f88d3ca8a2ebe6f208a864cc
rong5026 <rong5026@naver.com>
2024-09-03 14:56:01 +0900

    log파일 없으면 생성

--- config/log.py
+++ config/log.py
@@ -8,6 +8,11 @@ load_dotenv()
 # 로그 파일 경로 설정
 LOG_FILE = os.getenv("LOG_FILE", "application.log")
 
+# 로그 파일의 디렉토리 경로를 확인하고 디렉토리 생성
+log_dir = os.path.dirname(LOG_FILE)
+if log_dir and not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
 # 로그 파일이 없으면 생성
 if not os.path.exists(LOG_FILE):
     open(LOG_FILE, 'a').close()
