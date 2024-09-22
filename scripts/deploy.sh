0f729d8c5423707b96cd3a0bb6b3b04bc2d635d0
rong5026 <rong5026@naver.com>
2024-09-22 22:44:16 +0900

    log 볼륨 마운트 , main.py 제거

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,8 +19,7 @@ if [ ! -f "$MUSINSA_LOG_FILE" ]; then
 fi
 
 # cronjob 추가 (오전 6시 한국 시간에 실행)
-# echo "0 6 * * * root python /app/musinsa/product_day_price.py >> /app/log/musinsa_log.log 2>&1" >> /etc/crontab
-echo "* * * * * root python /app/musinsa/product_day_price.py >> /app/log/musinsa_log.log 2>&1" >> /etc/crontab
+echo "0 6 * * * root python /app/musinsa/product_day_price.py >> /app/log/musinsa_log.log 2>&1" >> /etc/crontab
 
 # cron 데몬 시작
 cron && tail -f "$MUSINSA_LOG_FILE"
